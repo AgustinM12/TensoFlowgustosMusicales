e2c0fa6b0cad1c7a36720f890f2ef178a3ef783c
AgustinM12 <agustinmazza12345@gmail.com>
2024-04-25 21:43:49 -0300

    FEAT: se agrego el calculo del genero favorito con tensorFlow

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const artists = document.getElementById("artists")
 // const welcome = document.getElementById("welcome")
 const calcularBtn = document.getElementById("calcularBtn")
 // welcome.innerHTML = `¡Hola ${userName}! Califica los siguientes grupos musicales y te diremos tus generos musicales favoritos.`
+const generoRecomendado = document.getElementById("generoRecomendado")
 
 const artistList = [
     "Gun's and Roses",
@@ -22,7 +23,7 @@ const artistList = [
     "System of a Down",
 
     "Rata Blanca",
-    "Los Autenticos Decadentes",
+    "Soda Stereo",
     "Vilma Palma e Vampiros",
 ]
 
@@ -35,7 +36,7 @@ const generos = [
 ]
 
 function generateOptions() {
-    let options = '<option disabled selected>Seleccione una puntuación</option>';
+    let options = '<option disabled selected>Puntuación</option>';
     for (let i = 1; i <= 10; i++) {
         options += `<option value="${i}">${i}</option>`;
     }
@@ -43,8 +44,9 @@ function generateOptions() {
 }
 
 artists.innerHTML = artistList.map(artist =>
-    `<li class="mx-auto">${artist}
-    <select>
+    `<li class="flex justify-between">
+    <p class="font-bold text-white">${artist}</p>
+    <select class="w-25 rounded-md bg-blue-200 hover:bg-blue-500 transition-colors">
     ${generateOptions()}   
     </select>
     </li>
@@ -70,8 +72,49 @@ calcularBtn.addEventListener("click", () => {
         return; // Detiene la ejecución si hay algún valor inválido
     }
 
-    console.log(values);
+    tf.tidy(() => {
+        // Crear un tensor con los las opciones elegidas por el usuario
+        const user_votes = tf.tensor([values])
+
+        // Clasificación de los artistas
+        const band_feats = tf.tensor([
+            [1, 0, 0, 0, 0],
+            [1, 0, 0, 0, 0],
+            [1, 0, 0, 0, 0],
+
+            [0, 1, 0, 0, 0],
+            [0, 1, 0, 0, 0],
+            [0, 1, 0, 0, 0],
+
+            [0, 0, 1, 0, 0],
+            [1, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+
+            [0, 0, 0, 1, 0],
+            [1, 0, 0, 1, 0],
+            [1, 0, 0, 1, 0],
+
+            [0, 0, 0, 0, 1],
+            [0, 0, 0, 1, 1],
+            [0, 0, 0, 0, 1]
+
+        ])
+
+        // Multiplicar los gustos del usuario por las características de los artistas
+        const recomendaciones = tf.matMul(user_votes, band_feats)
+
+        // Devolver un tensor con los valores máximos junto con su indice
+        const MejoresRecomendacionesPU = tf.topk(recomendaciones, generos.length)
+        // Obtener los valores de los indices en un array sincrono
+        const MejoresGeneros = MejoresRecomendacionesPU.indices.arraySync()
+
+        generoRecomendado.innerHTML = MejoresGeneros[0].map((genero, index) =>
+            `
+           <li>
+                <h2 class="text-white font-bold ${index == 0 && "text-green-400"}">genero: ${generos[genero]}</h2>
+                <p class="text-white font-semiBold ${index == 0 && "text-green-400"}">puntuacion: ${MejoresRecomendacionesPU.values.arraySync()[0][index]}</p>
+            </li>
+            `
+        ).join("")
+    })
 });
-
-
-
